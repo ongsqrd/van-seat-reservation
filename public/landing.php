6e6f55b259918f7cc4112dd95be1969f4e0c138d
u6711038 <u6711038@au.edu>
2026-09-01 11:42:35 +0700

    adding landing.php html

--- public/landing.php
+++ public/landing.php
@@ -1,22 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>AU VAN - Landing Page</title>
     <link rel="stylesheet" href="css/style.css">
 </head>
-<body>
-    <div class="header-centered">
-        <h2>AU VAN</h2>
-    </div>
 
-    <div class="login-container">
-        <form action="login_process.php" method="POST">
-            <div class="form-group">
-                Test
+<body>
+            <h1>AU VAN<br>BOOKING</h1>
+
+            <p>
+                Welcome to our AU Van booking service.
+                Where would you like to go today? If you are
+                new here, click Join Now to sign up and
+                make an account.
+            </p>
+
+            <div class="button-container">
+                <button type="button" class="join-button">Join now</button>
+                <button type="button" class="login-button">Log in</button>
             </div>
-        </form>
-    </div>
 </body>
 </html>
