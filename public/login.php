9699cf7bb066a9cc896ae13c36997c377bd7e8eb
dehhrlon <saengxuto.c@gmail.com>
2026-09-01 14:10:21 +0700

    finished login.php html and css

--- public/login.php
+++ public/login.php
@@ -7,32 +7,32 @@
   <title>Login</title>
 </head>
 <body>
-    <div class="header-centered">
-        <h2>Welcome Back!</h2>
-    </div>
+    <main>
+        <section class="login-section">
+        <div class="header-centered">
+            <h2>Welcome Back!</h2>
+        </div>
 
-    <div class="login-container">
-        <form action="login_process.php" method="POST">
-            <div class="form-group">
-                <label for="phone">Phone:</label>
-                <input type="tel" id="phone" name="phone" required>
-            </div>
+        <div class="login-container">
+            <form action="login_process.php" method="POST">
+                <div class="form-group">
+                    <label for="phone">PHONE</label>
+                    <input type="tel" id="phone" name="phone" value="" class="input-field" required>
+                </div>
 
-            <div class="form-group">
-            <label for="password">Password</label>
-            <input type="password" id="password" name="password" required>
-            </div>
-            <button type="submit" class="btn-primary btn-centered">Login</button>
-            <br>
-            <div class="navlink-centered">
-                <label for="no-account">Don't have an account? <a href="register.php">Register here</a></label>
-            </div>
-        </form>
+                <div class="form-group">
+                    <label for="password">PASSWORD</label>
+                    <input type="password" id="password" name="password" value="" class="input-field" required>
+                </div>
 
-        <p class="form-footer">
-            Don't have an account? <a href="register.php">Register here</a>
-        </p>
-    </section>
+                <button type="submit" class="btn-primary btn-centered">Login</button>
+
+                <div class="navlink-centered">
+                    <p class="no-account">Don't have an account? <a href="register.php">Register here</a></p>
+                </div>
+            </form>
+        </div>
+        </section>
   </main>
 </body>
 </html>
